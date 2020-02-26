c17aca6a1f29fbacb2dea4669970888c4e515e68
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2020-02-25 21:28:14 -0300

    Correção de Erros

--- SortApp/www/js/implementa.js
+++ SortApp/www/js/implementa.js
@@ -19,13 +19,18 @@ var SortApp;
         });
         var mostrarPessoas = document.getElementById("btnMostrarPessoas");
         mostrarPessoas.addEventListener('click', function () {
-            var pessoas = Sorteador.getPessoas();
             var mostrandoPessoas = document.getElementById('mostrandoPessoas');
-            var listaPessoas = '';
-            pessoas.forEach(function (pessoa) {
-                listaPessoas += "\n                <a href=\"#\" class=\"list-group-item list-group-item-action\">\n                    <div class=\"d-flex w-100 justify-content-between\">\n                        <h5 class=\"mb-1\">" + pessoa.getNome() + "</h5>\n                        <small class=\"text-muted\">" + pessoa.getIdade() + " anos</small>\n                    </div>\n                    <p class=\"mb-1\">\n                        " + pessoa.getPerfil() + "\n                    </p>\n                </a>\n               ";
-            });
-            mostrandoPessoas.innerHTML = listaPessoas;
+            var pessoas = Sorteador.getPessoas();
+            if (pessoas.length > 0) {
+                var listaPessoas_1 = '';
+                pessoas.forEach(function (pessoa) {
+                    listaPessoas_1 += "\n                <a href=\"#\" class=\"list-group-item list-group-item-action\">\n                    <div class=\"d-flex w-100 justify-content-between\">\n                        <h5 class=\"mb-1\">" + pessoa.getNome() + "</h5>\n                        <small class=\"text-muted\">" + pessoa.getIdade() + " ano(s)</small>\n                    </div>\n                    <p class=\"mb-1\">\n                        " + pessoa.getPerfil() + "\n                    </p>\n                </a>\n               ";
+                });
+                mostrandoPessoas.innerHTML = listaPessoas_1;
+            }
+            else {
+                mostrandoPessoas.innerHTML = "\n                <a href=\"#\" class=\"list-group-item list-group-item-action\">\n                    <div class=\"d-flex w-100 justify-content-between\">\n                        <h5 class=\"mb-1\">Cadastre pelo menos uma pessoa</h5>\n                    </div>\n                </a>\n                ";
+            }
         });
         var sorteio = document.getElementById('btnSorteio');
         sorteio.addEventListener('click', function () {
--- SortAppTS/implementa.ts
+++ SortAppTS/implementa.ts
@@ -23,33 +23,43 @@ namespace SortApp {
         let mostrarPessoas = < HTMLButtonElement > document.getElementById("btnMostrarPessoas");
 
         mostrarPessoas.addEventListener('click', function () {
-            let pessoas = Sorteador.getPessoas();
             let mostrandoPessoas = < HTMLDivElement > document.getElementById('mostrandoPessoas');
-            var listaPessoas = '';
-            pessoas.forEach(function (pessoa) {
-                listaPessoas += `
+            let pessoas = Sorteador.getPessoas();
+            if (pessoas.length > 0) {
+                let listaPessoas = '';
+                pessoas.forEach(function (pessoa) {
+                    listaPessoas += `
                 <a href="#" class="list-group-item list-group-item-action">
                     <div class="d-flex w-100 justify-content-between">
-                        <h5 class="mb-1">`+ pessoa.getNome() +`</h5>
-                        <small class="text-muted">`+ pessoa.getIdade() +` anos</small>
+                        <h5 class="mb-1">` + pessoa.getNome() + `</h5>
+                        <small class="text-muted">` + pessoa.getIdade() + ` ano(s)</small>
                     </div>
                     <p class="mb-1">
-                        `+ pessoa.getPerfil() +`
+                        ` + pessoa.getPerfil() + `
                     </p>
                 </a>
                `
-            })
-            mostrandoPessoas.innerHTML = listaPessoas;
+                })
+                mostrandoPessoas.innerHTML = listaPessoas;
+            }else{
+                mostrandoPessoas.innerHTML = `
+                <a href="#" class="list-group-item list-group-item-action">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1">Cadastre pelo menos uma pessoa</h5>
+                    </div>
+                </a>
+                `;
+            }
         });
 
         let sorteio = < HTMLButtonElement > document.getElementById('btnSorteio');
 
-        sorteio.addEventListener('click', function(){
+        sorteio.addEventListener('click', function () {
             let pessoaSorteadaHeading = < HTMLHeadingElement > document.getElementById('pessoaSorteada');
-            if(Sorteador.sortearPessoa()){
+            if (Sorteador.sortearPessoa()) {
                 let pessoaSorteada = Sorteador.sortearPessoa().getNome();
                 pessoaSorteadaHeading.innerHTML = pessoaSorteada;
-            }else{
+            } else {
                 pessoaSorteadaHeading.innerHTML = 'Cadastre pelo menos 1 pessoa';
             }
         });
